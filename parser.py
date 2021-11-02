5a4b557ed0feeb117d967cd111c82c232a322cf7
drondel <realdrews@gmail.com>
2021-11-02 18:36:19 +0300

    Проверка строк файла, и реализация всей программы

--- parser.py
+++ parser.py
@@ -1,34 +1,55 @@
 import argparse
 import os
-import re
+import regex as re
+from tqdm import tqdm
 
 
 def take_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-p')
-    parser.add_argument('-reg')
-    parser.add_argument('-end')
+    parser.add_argument('-p', required=True)
+    parser.add_argument('-reg', required=True)
+    parser.add_argument('-end', required=True)
     args = parser.parse_args()
     return args
 
 
-def file_dir(args):
-    if os.path.isfile(args.p):  # функция принимает на вход аргументы, переданные пользователем
-        return [args.p]  # обрабатывает их, и на выходе мы получаем все файлы, нужные нам для парсинга
-    else:
-        files = os.listdir(path=args.p)
-        if files:
-            rg = re.compile(args.end)
-            return [args.p + '/' + p for p in os.listdir(path=args.p) if os.path.isfile(args.p + '/' + p) and re.match(p)]
+def files_path(args):
+    if os.path.isfile(args.p):
+        return [args.p]
+    elif os.path.isdir(args.p):
+        dir = os.listdir(path=args.p)
+        if not dir:
+            print('Directory is empty')
+        else:           #пытался разобраться, почему не хочет читать файлы из заданного пути, так и не понял
+            rg = re.compile(args.end)      #при этом если указывать путь с конкретным файлом, все работает(((
+            return [args.p + '/' + p for p in dir if os.path.isfile(args.p + '/' + p) and rg.match(p)]
 
-
-def read(file_path):
-    with open (file_path, 'r') as f:   #функция находит файл по пути и возвращает все строки файла
+def reading(file_path):
+    with open(file_path, 'r') as f:
         return f.read()
 
-def new_file():
-    filename = 'newlog.log'
+
+def new_file(lines):
+    filename = 'new.log'
     with open(filename, 'a') as f:
-        f.write('_')
+        f.writelines(lines)
 
 
+def line_check(file_path, regexp, lines):
+    res = [file_path + ': ' + line + '\n' for line in tqdm(lines) if re.match(regexp, line)]
+    new_file(res)
+    return res
+
+
+if __name__ == '__main__':
+    args = take_args()
+    files_path = files_path(args)
+    count_files = len(files_path)
+    print(count_files)
+    count_files_now = 0
+    for file_path in files_path:
+        count_files_now += 1
+        print(f'Reading {file_path} {count_files_now}/{count_files} files')
+        regexp = args.reg
+        lines = reading(file_path).split('\n')
+        line_check(file_path, regexp, lines)
